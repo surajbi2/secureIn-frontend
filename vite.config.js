4f6e61a344a428a327eaff4d3a0e245f5c55d5dc
Ramagiri Suraj kumar <ramagirisurajkumar@gmail.com>
2025-06-05 17:26:28 +0530

    added vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
   server: {
-    host: true, // enables access from mobile
-    historyApiFallback: true // handle client-side routing
+    host: true
   },
   preview: {
-    host: true,
-    historyApiFallback: true
+    host: true
   }
 })
