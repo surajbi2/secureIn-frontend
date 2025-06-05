6b72927f1d759355d770dc768e7bfd6b26e64933
Ramagiri Suraj kumar <ramagirisurajkumar@gmail.com>
2025-06-05 17:29:47 +0530

    updated missing import

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    tailwindcss(),
+  ],
   server: {
-    host: true
+    host: true, // enables access from mobile
+    historyApiFallback: true // handle client-side routing
   },
   preview: {
-    host: true
+    host: true,
+    historyApiFallback: true
   }
 })
