57ba3c4a926831cf40fb9a3d27ec94a16f6a3b9f
Ramagiri Suraj kumar <ramagirisurajkumar@gmail.com>
2025-06-08 23:01:56 +0530

    changed path

--- src/path/apiPath.js
+++ src/path/apiPath.js
@@ -1,2 +1,2 @@
-export const API_PATH = "http://localhost:5000"
-// export const API_PATH = "https://securein-backend.onrender.com"
+// export const API_PATH = "http://localhost:5000"
+export const API_PATH = "https://securein-backend.onrender.com"
